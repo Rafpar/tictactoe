16c8a4fde04b405a0c7f78272d06632a3cb586fb
Rafal Parda <pardarafal@gmail.com>
2021-10-20 00:37:42 +0200

    refactoring

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -21,12 +21,8 @@ export class BoardController {
   @Get()
   async getBoard(@Res({ passthrough: true }) res: Response) {
     if (!(await this.boardService.isGameStarted())) {
-      res
-        .status(HttpStatus.NOT_FOUND)
-        .json({
-          message: 'Game is not started, please start the game',
-        })
-        .send();
+      const message = 'Game is not started, please start the game';
+      this.sendResponse(res, HttpStatus.NOT_FOUND, message);
       return;
     } else {
       return this.boardService.renderBoard();
@@ -40,21 +36,13 @@ export class BoardController {
     @Res({ passthrough: true }) res: Response,
   ) {
     if (!(await this.boardService.isGameStarted())) {
-      res
-        .status(HttpStatus.NOT_FOUND)
-        .json({
-          message: 'Game is not started, please start the game',
-        })
-        .send();
+      const message = 'Game is not started, please start the game';
+      this.sendResponse(res, HttpStatus.NOT_FOUND, message);
       return;
     }
     if (await this.boardService.isBoardLocked()) {
-      res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({
-          message: 'Board is locked!, Game is finished!',
-        })
-        .send();
+      const message = 'Board is locked!, Game is finished!';
+      this.sendResponse(res, HttpStatus.BAD_REQUEST, message);
       return;
     }
     if (
@@ -62,12 +50,8 @@ export class BoardController {
         updateBoardDto.playerName,
       ))
     ) {
-      res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({
-          message: 'It is not ' + updateBoardDto.playerName + ' turn',
-        })
-        .send();
+      const message = 'It is not ' + updateBoardDto.playerName + ' turn';
+      this.sendResponse(res, HttpStatus.BAD_REQUEST, message);
       return;
     }
     if (!(await this.boardService.isFieldAlreadyFilled(fieldNumber))) {
@@ -78,34 +62,27 @@ export class BoardController {
       if (await this.boardService.isGameFinished(fieldNumber)) {
         await this.boardService.lockBoard();
         if (await this.boardService.isWinner(fieldNumber)) {
-          res
-            .status(HttpStatus.OK)
-            .json({
-              message:
-                'Game finished ' + updateBoardDto.playerName + ' won the game!',
-            })
-            .send();
+          const message = 'Game finished ' + updateBoardDto.playerName + ' won the game!';
+          this.sendResponse(res, HttpStatus.OK, message);
+          return; }
+        else {
+          const message = 'Game finished, it is a draw!';
+          this.sendResponse(res, HttpStatus.OK, message);
           return;
-        } else {
-          res
-            .status(HttpStatus.OK)
-            .json({
-              message: 'Game finished, it is a draw!',
-            })
-            .send();
         }
       }
-      return board;
-    } else {
-      res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({
-          message:
-            'Field ' +
-            fieldNumber +
-            ' already filled or does not exist, choose other field',
-        })
-        .send();
+      return board; }
+    else {
+      const message = 'Field ' + fieldNumber + ' already filled or does not exist, choose other field';
+      this.sendResponse(res, HttpStatus.BAD_REQUEST, message);
     }
+  }
+  sendResponse(res: Response, httpStatus: HttpStatus, message: string) {
+    res
+      .status(httpStatus)
+      .json({
+        message: message,
+      })
+      .send();
   }
 }
